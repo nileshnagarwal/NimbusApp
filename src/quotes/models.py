4e93f14f10fdb4822014315d5c8cf671b69ff551
Nilesh Agarwal <eternalshenron@gmail.com>
2017-07-30 13:18:59 +0530

    Added comments to models.py for better readability.

--- src/quotes/models.py
+++ src/quotes/models.py
@@ -2,14 +2,19 @@ from django.db import models
 from vehicles.models import vehicle_type, vehicle_body
 from datetime import date
 
-# Create your models here.
+# Creating enquiry model.
 class enquiry(models.Model):
+    
+    # Defining choices for status and load type fields.
+    # Status choice fields
     Enquiry = 'EQ'
     FinalisedOrder = 'FO'
     status_choices = (
         (Enquiry, 'Enquiry'),
         (FinalisedOrder, 'Finalised Order'),
     )
+    
+    # Load type choice fields
     ODC = 'ODC'
     Normal = 'FTL'
     Part = 'LTL'
@@ -20,6 +25,8 @@ class enquiry(models.Model):
         (Part, 'Part'),
         (Container, 'Container'),
     )
+
+    # Defining model fields
     status = models.CharField(max_length=2, choices=status_choices,
                               default=Enquiry, blank=False)
     length = models.DecimalField(max_digits=5, decimal_places=2)
@@ -29,6 +36,11 @@ class enquiry(models.Model):
     source = models.CharField(max_length=255)
     destination = models.CharField(max_length=255)
     return_location = models.CharField(max_length=255)
+    
+    # Since vehicle_type and vehicle_body may be required to alter from admin
+    # panel for multiple times in the future, we are defining them as seperate
+    # models and using ForeignKey to connect the models in many to one
+    # relationship.
     vehicle_type = models.ForeignKey('vehicles.vehicle_type')
     vehicle_body = models.ForeignKey('vehicles.vehicle_body')
     load_type = models.CharField(max_length=3, choices=load_type_choices,
