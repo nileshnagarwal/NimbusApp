61cefdb5438406cb98d78a98b0bb75f6bf603b93
Nilesh Agarwal <eternalshenron@gmail.com>
2019-02-21 13:58:04 +0530

    Username changed to Email. Changes reflected in Admin Panel.

--- src/common/admin.py
+++ src/common/admin.py
@@ -1,6 +1,33 @@
+"""Admin Panel for Common Module's Models"""
+
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 from .models import User
 
-# Register your models here.
-admin.site.register(User, UserAdmin)
+"""Since we have removed Username field from User Model and added email
+field, we need to make changes to UserAdmin to be able to use it in
+admin panel.
+Refer: https://www.fomfus.com/articles/how-to-use-email-as-username (cont)
+-for-django-authentication-removing-the-username"""
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'user_type')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'user_type')
+    search_fields = ('email', 'first_name', 'last_name', 'user_type')
+    ordering = ('email',)
--- src/common/models.py
+++ src/common/models.py
@@ -1,10 +1,49 @@
+"""Here we extend and alter the Base User Model of Django. We remove the Username
+field and make email the primary unique field.
+Refer: https://www.fomfus.com/articles/how-to-use-email-as-username-for (cont)
+-django-authentication-removing-the-username
+"""
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 # Create your models here.
 class User(AbstractUser):
     """Base User Model + userType field"""
-    
+
     USERTYPE_CHOICES = (
         (1, 'Developer'),
         (2, 'Admin'),
@@ -12,4 +51,16 @@ class User(AbstractUser):
         (4, 'Traffic Incharge'),
         (5, 'Accounts'),
     )
+
+    username = None
+
     user_type = models.IntegerField(choices=USERTYPE_CHOICES, default=4)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
