fa674da94efc1ac91954ef1486b8e8ffccc94897
Nilesh Agarwal <eternalshenron@gmail.com>
2019-11-29 13:25:04 +0530

    Pagination added

--- masters/views.py
+++ masters/views.py
@@ -13,73 +13,87 @@ class VehicleTypeList(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = VehicleType.objects.all().order_by('-vehicle')
     serializer_class = VehicleTypeSerializer
-
+    pagination_class = None
 
 class VehicleTypeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = VehicleType.objects.all()
     serializer_class = VehicleTypeSerializer
+    pagination_class = None
 
 class VehicleBodyList(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = VehicleBody.objects.all().order_by('-vehicle_body_id')
     serializer_class = VehicleBodySerializer
+    pagination_class = None
 
 class VehicleBodyDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = VehicleBody.objects.all()
     serializer_class = VehicleBodySerializer
+    pagination_class = None
 
 class LoadTypeList(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = LoadType.objects.all().order_by('-load_type')
     serializer_class = LoadTypeSerializer
-
+    pagination_class = None
 
 class LoadTypeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = LoadType.objects.all()
     serializer_class = LoadTypeSerializer
+    pagination_class = None
 
 class TransporterList(generics.ListCreateAPIView):
     queryset = Transporter.objects.all().order_by('transporter')
     serializer_class = TransporterSerializer
+    pagination_class = None
 
 class TransporterDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transporter.objects.all()
     serializer_class = TransporterSerializer
+    pagination_class = None
 
 class ExtraExpensesList(generics.ListCreateAPIView):
     queryset = ExtraExpenses.objects.all().order_by('extra_expenses')
     serializer_class = ExtraExpensesSerializer
+    pagination_class = None
 
 class ExtraExpensesDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ExtraExpenses.objects.all()
     serializer_class = ExtraExpensesSerializer
+    pagination_class = None
 
 class PlacesList(generics.ListCreateAPIView):
     """Creating List and Post functions for Places model"""
     queryset = Places.objects.all().order_by('place_id')
     serializer_class = PlacesSerializer
+    pagination_class = None
 
 class PlacesDetail(generics.RetrieveUpdateDestroyAPIView):
     """Creating RUD functions for Places model"""
     queryset = Places.objects.all()
     serializer_class = PlacesSerializer
+    pagination_class = None
 
 class DistrictList(generics.ListCreateAPIView):
     """Creating List and Post functions for District model"""
     queryset = District.objects.all().order_by('district_id')
     serializer_class = DistrictSerializer
+    pagination_class = None
 
 class DistrictDetail(generics.RetrieveUpdateDestroyAPIView):
     """Creating RUD functions for District model"""
     queryset = District.objects.all()
     serializer_class = DistrictSerializer
+    pagination_class = None
 
 class TransporterProfileList(generics.ListCreateAPIView):
     """Creating List and Post functions for District model"""
     queryset = TransporterProfile.objects.all().order_by('trans_profile_id')
     serializer_class = TransporterProfileSerializer
+    pagination_class = None
 
 class TransporterProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     """Creating RUD functions for District model"""
     queryset = TransporterProfile.objects.all()
     serializer_class = TransporterProfileSerializer
+    pagination_class = None
