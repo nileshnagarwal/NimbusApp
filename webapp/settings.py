a5a27ebf166e5c4043cd43ec3a9a9335caddcb23
Nilesh Agarwal <eternalshenron@gmail.com>
2020-01-18 21:00:57 +0530

    Static Settings Changes

--- webapp/settings.py
+++ webapp/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = ('localhost:8080', 'localhost:3000', \
